93c52721c6ba75ff68b89cb765e2a01c94e5a715
dreat <lukasz@hawku.com>
2023-09-01 16:13:50 +0200

    fix build

--- src/components/Gdzie.js
+++ src/components/Gdzie.js
@@ -18,18 +18,6 @@ function Gdzie() {
             <p className='pt-5'>Zapewnimy wam miejsce do pracy, prąd, wi-fi... + jedzenie i picie.</p>
           </div>
         </div>
-      <!--
-        <div className='pt-10 sm:pt-20'>
-          <div className='sm:flex items-center justify-center'>
-          <h3 className='text-xl md:text-2xl lg:text-3xl sm:pr-10'>Afterparty</h3>
-          <a href='https://www.google.com/maps/place/Czudner+SPOT/@54.3413672,18.6551715,17z/data=!3m1!4b1!4m6!3m5!1s0x46fd73ee018180cd:0xdb515aa03930f377!8m2!3d54.3413642!4d18.6600478!16s%2Fg%2F11t321fbtq?entry=ttu' target='_blank'>
-            <button className='mt-2 sm:mt-0 font-semibold bg-red-600 rounded-md px-4 py-1 md:py-2 hover:scale-105 duration-500 text-xl md:text-2xl lg:text-3xl'>Zobacz na mapie</button>
-          </a>          </div>
-          <div className='text-lg md:text-xl lg:text-2xl font-light'>
-            <p className='pt-5'>Czudner SPOT, ul. Łąkowa 35, Gdańsk, 22.00 - koncert Searching for Managment, potem afterparty - dla uczestników wejście free!</p>
-          </div>
-        </div> -->
-        
     </div>
   )
 }
